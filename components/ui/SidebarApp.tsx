5049dfdfdd070f25b76c22b544025b720578c7ed
Danilo Calderon <Danilo0203@users.noreply.github.com>
2024-09-15 20:13:38 -0600

    [ADD]: Agregar link del sidebar y dashboard

---
+++ components/ui/SidebarApp.tsx
@@ -0,0 +1,76 @@
+"use client";
+import { CompassIcon, HeartIcon, HouseIcon, UserIcon } from "lucide-react";
+import { useState } from "react";
+import { Sidebar, SidebarBody, SidebarLink } from "./sidebar";
+import Link from "next/link";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { Dashboard } from "./Dashboard";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+
+export const SidebarApp = () => {
+  const links = [
+    {
+      label: "Inicio",
+      href: "/",
+      icon: <HouseIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+      label: "Favoritos",
+      href: "/favoritos",
+      icon: <HeartIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+      label: "Explorar",
+      href: "/explorar",
+      icon: <CompassIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+    {
+      label: "Perfil",
+      href: "/perfil",
+      icon: <UserIcon className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />,
+    },
+  ];
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="flex flex-col md:flex-row h-screen">
+      <Sidebar open={open} setOpen={setOpen}>
+        <SidebarBody className="justify-between gap-10">
+          <div className="flex flex-col flex-[1] overflow-y-auto overflow-x-hidden">
+            {open ? <Logo /> : <LogoIcon />}
+            <div className="mt-8 flex flex-col gap-2">
+              {links.map((link, idx) => (
+                <SidebarLink key={`${idx}-${link.label}`} link={link} />
+              ))}
+            </div>
+          </div>
+          <div>
+            <ThemeSwitcher />
+          </div>
+        </SidebarBody>
+      </Sidebar>
+      <Dashboard />
+    </div>
+  );
+};
+export const Logo = () => {
+  return (
+    <Link href="#" className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+      <motion.span
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="font-medium text-black dark:text-white whitespace-pre"
+      >
+        Flash Cards
+      </motion.span>
+    </Link>
+  );
+};
+export const LogoIcon = () => {
+  return (
+    <Link href="#" className="font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20">
+      <div className="h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0" />
+    </Link>
+  );
+};
---
+++ components/ui/ThemeSwitcher.tsx
@@ -0,0 +1,31 @@
+// app/components/ThemeSwitcher.tsx
+"use client";
+
+import { Button } from "@nextui-org/button";
+import { MoonIcon, SunIcon } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      {/* <div>
+        The current theme is: {theme}
+        <button onClick={() => setTheme("light")}>Light Mode</button>
+        <button onClick={() => setTheme("dark")}>Dark Mode</button>
+      </div> */}
+      <Button onClick={() => setTheme(theme !== "dark" ? "dark" : "light")} isIconOnly>
+        {theme === "dark" ? <MoonIcon /> : <SunIcon />}
+      </Button>
+    </>
+  );
+}
